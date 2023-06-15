05e2bd3c09e602b7d5521db3356b7a63533ef9ae
webbroy <46152164+webbroy@users.noreply.github.com>
2023-06-15 15:36:58 -0700

    Update App.jsx

--- class/my-app/src/App.jsx
+++ class/my-app/src/App.jsx
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// return (
+	//   <div className="App">
+	//     <header className="App-header">
+	//       <img src={logo} className="App-logo" alt="logo" />
+	//       <p>
+	//         Edit <code>src/App.js</code> and save to reload.
+	//       </p>
+	//       <a
+	//         className="App-link"
+	//         href="https://reactjs.org"
+	//         target="_blank"
+	//         rel="noopener noreferrer"
+	//       >
+	//         Learn React
+	//       </a>
+	//     </header>
+	//   </div>
+	// );
+	const name = "jake";
+	const element = <h1> Hello, {name}</h1>;
+	const element2 = (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+	console.log("element", element);
+	console.log("element2", element2);
+
+	return element;
 }
 
 export default App;
