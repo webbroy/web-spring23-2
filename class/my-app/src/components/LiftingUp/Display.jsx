162ae61075a43a02401f803b55bc93d24a8bf19e
webbroy <46152164+webbroy@users.noreply.github.com>
2023-07-21 11:59:24 -0700

    update

---
+++ class/my-app/src/components/LiftingUp/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  = () => {
+  return (
+    <div>
+        <h2>text form input:</h2>
+    </div>
+  );
+};
+
+export default Display;
---
+++ class/my-app/src/components/LiftingUp/Form.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Form = () => {
+	return (
+		<div>
+			<Input />
+		</div>
+	);
+};
+export default Form;
