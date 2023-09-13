3142c915f967ec849c02b81130b18b6d9d03b688
webbroy <46152164+webbroy@users.noreply.github.com>
2023-09-12 18:10:38 -0700

    update

---
+++ class/js/twoSum.js
@@ -0,0 +1,19 @@
+// //two sum
+
+// function twoSum(nums, target){
+//     l = 0;
+//     r= nums.length-1;
+//     while((l+r) = target);{
+
+//         if (l + r) is > target; {
+//             l++;
+//         }elsel+r < target;{
+//             r--
+//         }
+//     }
+//     return[l,r]
+// }console.log(twoSum([1,5,3,,5,4,7,11], 9));
+
+var myName = "anna";
+let age = 20; // this results in compile-time error
+console.log(age);
---
+++ class/my-app/src/components/LiftingUp/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const LiftingUp = () => {
+	return (
+		<div>
+			<h1>Lifting State</h1>
+			<Form />
+
+			<br />
+
+			<Display />
+			<Display />
+		</div>
+	);
+};
+
+export default LiftingUp;
